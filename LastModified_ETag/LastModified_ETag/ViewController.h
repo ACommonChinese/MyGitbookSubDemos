211e54786320e25269684b6d21afe0712e355cf1
刘威振 <weizhen.lwz@alibaba-inc.com>
2019-05-30 16:40:46 +0800

    add LastModified_ETag

---
+++ LastModified_ETag/LastModified_ETag/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LastModified_ETag
+//
+//  Created by liuweizhen on 2019/5/30.
+//  Copyright © 2019 DaLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LastModified_ETag/LastModified_ETag/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LastModified_ETag
+//
+//  Created by liuweizhen on 2019/5/30.
+//  Copyright © 2019 DaLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
